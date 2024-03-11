ca68ca7625c9a6e50b95ae215cd7c5491ce4fce3
Illia Dyrda <106555644+dyrdaillia@users.noreply.github.com>
2024-03-11 11:12:26 +0100

    Add files via upload

---
+++ FileController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Text;
+
+namespace WebApplication7.Controllers
+{
+    public class FileController : Controller
+    {
+        [HttpGet]
+        public IActionResult DownloadFile()
+        {
+            // Відображення HTML-форми для вводу даних
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DownloadFile(string firstName, string lastName, string fileName)
+        {
+            // Створення текстового файлу з введеними даними
+            var filePath = $"{fileName}.txt";
+            var fileContent = $"Ім'я: {firstName}\nПрізвище: {lastName}";
+
+            // Запис вмісту у файл
+            System.IO.File.WriteAllText(filePath, fileContent, Encoding.UTF8);
+
+            // Повернення файлу користувачу для завантаження
+            return PhysicalFile(filePath, "text/plain", fileName + ".txt");
+        }
+    }
+}
